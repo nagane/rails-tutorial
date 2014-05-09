f5a7a8008517ce08d57893cf33f5cd20d9010754
nagane <naganeys@gmail.com>
2014-05-09 22:05:18 +0900

    Shared Examples機能導入

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,33 +6,38 @@ describe "Static Pages" do
   # should呼び出しは自動的にCapybaraにより提供されるpage変数を仕様する
   subject { page }
 
+  shared_examples_for "all static pages" do
+    it { should have_content(heading) }
+    it { should have_title(page_title) }
+  end
+  
   describe "Home page" do
     before { visit root_path }
-
-    it { should have_content('Sample App') }
-    it { should have_title("Home") }
+    let(:heading)   { 'Sample App' }
+    let(:page_title) { 'Home' }
+    it_should_behave_like "all static pages"
     it { should_not have_title("Sample App") }
   end
   
   describe "Help page" do
     before { visit help_path }
-
-    it { should have_content('Help') }
-    it { should have_title("Help") }
+    let(:heading)   { 'Help' }
+    let(:page_title) { 'Help' }
+    it_should_behave_like "all static pages"
   end
 
   describe "About page" do
     before { visit about_path }
-
-    it { should have_content('About') }
-    it { should have_title("About") }
+    let(:heading)   { 'About' }
+    let(:page_title) { 'About' }
+    it_should_behave_like "all static pages"
   end
 
   describe "Contact page" do
     before { visit contact_path }
-
-    it { should have_content('Contact') }
-    it { should have_title("Contact") }
+    let(:heading)   { 'Contact' }
+    let(:page_title) { 'Contact' }
+    it_should_behave_like "all static pages"
   end
 
 
