cc5f00167ed41ceda2c6d6d565b64a8402048af2
nagane <naganeys@gmail.com>
2014-05-13 21:53:10 +0900

    アドレスのユニークテスト実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   has_many :microposts
   validates :name, presence: true, length: { maximum: 50 }
+  VAILD_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true
 end
