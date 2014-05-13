26fdcd0cad2785f68bf7ffea83f801aff13db4cb
nagane <naganeys@gmail.com>
2014-05-13 23:01:24 +0900

    DB登録前にemailを小文字に変換する これにより一意性を保証する
    チュートリアル6.2まで終了

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140407124855) do
+ActiveRecord::Schema.define(version: 20140513134959) do
 
   create_table "ipv4admins", force: true do |t|
     t.integer  "ip_status"
@@ -38,5 +38,7 @@ ActiveRecord::Schema.define(version: 20140407124855) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
