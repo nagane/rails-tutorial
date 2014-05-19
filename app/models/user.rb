914b5f4561e3ce4b756106d88251fe44a78fced7
nagane <naganeys@gmail.com>
2014-05-19 23:23:12 +0900

    has_secure_passwordとそれに関連するテスト追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ActiveRecord::Base
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   before_save { self.email = email.downcase }
+  has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140513134959) do
+ActiveRecord::Schema.define(version: 20140519140414) do
 
   create_table "ipv4admins", force: true do |t|
     t.integer  "ip_status"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20140513134959) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
