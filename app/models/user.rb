a4b986a73ccdb889c603656eae9b284940954ac7
nagane <naganeys@gmail.com>
2014-05-20 22:56:01 +0900

    コメントアウト解除

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-  #before_save { self.email = email.downcase }
+  before_save { self.email = email.downcase }
   # セキュアなパスワードを実装するメソッド
   has_secure_password
   validates :password, length: { minimum: 6 }
